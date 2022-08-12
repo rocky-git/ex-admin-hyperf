758f537378e62c02e3108b781a7bd1be6d9a0aa8
rocky-git <631670747@qq.com>
2022-08-12 15:04:04 +0800

    error返回异常

--- src/ExAdminExceptionHandler.php
+++ src/ExAdminExceptionHandler.php
@@ -1,20 +1,28 @@
 <?php
 namespace ExAdmin\hyperf;
 use ExAdmin\hyperf\exception\HttpResponseException;
+use ExAdmin\ui\exception\MessageResponseException;
 use Hyperf\ExceptionHandler\ExceptionHandler;
+use Hyperf\HttpMessage\Stream\SwooleStream;
 use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\Utils\ApplicationContext;
+use Hyperf\Utils\Context;
 use Psr\Http\Message\ResponseInterface;
 use Hyperf\Di\Annotation\Inject;
-class ExAdminExceptionHandler extends  ExceptionHandler
+class ExAdminExceptionHandler extends ExceptionHandler
 {
     public function handle(\Throwable $throwable, ResponseInterface $response)
     {
+       
         // 判断被捕获到的异常是希望被捕获的异常
         if ($throwable instanceof HttpResponseException) {
             $response = $throwable->getResponse();
             // 阻止异常冒泡
             $this->stopPropagation();
-            return $response;
+        }elseif ($throwable instanceof MessageResponseException){
+            $response = $response->withAddedHeader('content-type', 'application/json')
+                ->withContent(json_encode($throwable->getResponse()));
+            $this->stopPropagation();
         }
         // 交给下一个异常处理器
         return $response;
